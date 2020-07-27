d5d40f1c88d9774e05aa7ce671bbee1e938c1f22
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-27 19:48:27 +0200

    del redundant comments

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,22 +7,3 @@ from django.contrib import admin
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
-
-# urlpatterns = format_suffix_patterns([
-#     path('admin/', admin.site.urls),
-#     path('snippets/',
-#          views.SnippetList.as_view(),
-#          name='snippet-list'),
-#     path('snippets/<int:pk>/',
-#          views.SnippetDetail.as_view(),
-#          name='snippet-detail'),
-#     path('snippets/<int:pk>/highlight/',
-#          views.SnippetHighlight.as_view(),
-#          name='snippet-highlight'),
-#     path('users/',
-#          views.UserList.as_view(),
-#          name='user-list'),
-#     path('users/<int:pk>/',
-#          views.UserDetail.as_view(),
-#          name='user-detail')
-# ])
